7fcd68fcdb888e76b38d4f4937f9638b3037700b
Simon Monette <monette.simon@gmail.com>
2016-02-20 15:08:36 -0500

    moved lines aroud so date wont cause error

--- .config/i3/custom-i3status.sh
+++ .config/i3/custom-i3status.sh
@@ -3,35 +3,33 @@
 
 i3status --config ~/.config/i3status/config | while :
 do
-        read line
-        
-        # Display cmus song
-        array="$(cmus-remote -Q)"
-        artist=$(echo "$array" | grep ' artist ' | cut -d ' ' -f3-)
-        song=$(echo "$array" | grep title | cut -d ' ' -f3-)
+	read line
+	
+	# Display cmus song
+	array="$(cmus-remote -Q)"
+	artist=$(echo "$array" | grep ' artist ' | cut -d ' ' -f3-)
+	song=$(echo "$array" | grep title | cut -d ' ' -f3-)
 
-        time1=$(echo "$array" | grep duration | cut -d ' ' -f2)
-        time2=$(echo "$array" | grep position | cut -d ' ' -f2)
+	time1=$(echo "$array" | grep duration | cut -d ' ' -f2)
+	time2=$(echo "$array" | grep position | cut -d ' ' -f2)
 
-        t1=$(date -u -d @${time1} +"%M:%S")
-        t2=$(date -u -d @${time2} +"%M:%S")
+	if [ "$time1" = "" ]; then
+		playing=""
+	else
+		t1=$(date -u -d @${time1} +"%M:%S")
+		t2=$(date -u -d @${time2} +"%M:%S")
+		playing="$t2/$t1 $song - $artist"
+		playing="${playing//&}"
+	fi
 
-        playing="$t2/$t1 $song - $artist"
+	# Display used RAM
+	mem=$(free -h| grep Mem| cut -d' ' -f20)
 
-        if [ "$t2" = "" ]; then
-            playing=""
-        else
-            playing="${playing//&}"
-        fi
+	# Display current brightness
+	x=$(cat /sys/class/backlight/acpi_video0/brightness)
+	x=$(echo "scale=2; $x/15 * 100"|bc)
+	brightness="${x%.*}%"
 
-        # Display used RAM
-        mem=$(free -h| grep Mem| cut -d' ' -f20)
-
-        # Display current brightness
-        x=$(cat /sys/class/backlight/acpi_video0/brightness)
-        x=$(echo "scale=2; $x/15 * 100"|bc)
-        brightness="${x%.*}%"
-
-        # Print final result
-        echo -n " $playing |  $mem |  $brightness | $line" || exit 1
+	# Print final result
+	echo -n " $playing |  $mem |  $brightness | $line" || exit 1
 done
