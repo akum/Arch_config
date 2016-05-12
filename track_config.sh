37d5a4eaa088c6a875dd1ac71b189b4e71e95905
Simon Monette <monette.simon@gmail.com>
2016-05-12 18:47:04 -0400

    Track new files

--- track_config.sh
+++ track_config.sh
@@ -4,22 +4,24 @@ gitf="$HOME/Arch_config"
 lbin="/usr/local/bin"
 service="systemd/system"
 
-cp ~/.bashrc		$gitf/
-cp ~/.gtkrc-2.0		$gitf/
-cp ~/.Xresources	$gitf/
+cp ~/.bashrc     $gitf/
+cp ~/.gtkrc-2.0  $gitf/
+cp ~/.Xresources $gitf/
 
-cp ~/.config/compton.conf			$gitf/.config/
-cp ~/.config/i3/config				$gitf/.config/i3/
-cp ~/.config/i3/custom-i3status.sh	$gitf/.config/i3/
-cp ~/.config/i3status/config		$gitf/.config/i3status/
-cp ~/.config/cmus/rc				$gitf/.config/cmus/
+cp ~/.config/i3/config    $gitf/.config/i3/
+cp ~/.i3blocks.conf       $gitf/
+cp ~/.config/compton.conf $gitf/.config/
+cp ~/.config/cmus/rc      $gitf/.config/cmus/
 
-cp /etc/default/grub				$gitf/etc/default/
-cp /etc/plymouth/plymouthd.conf		$gitf/etc/plymouth/
-cp /etc/mkinitcpio.conf				$gitf/etc/
-cp /etc/$service/lock.service		$gitf/etc/$service/
+cp /usr/lib/i3blocks/simplecal $gitf/usr/lib/i3blocks/
+cp /usr/lib/i3blocks/music     $gitf/usr/lib/i3blocks/
 
-cp $lbin/togglebluetooth.sh	$gitf/bin/
-cp $lbin/toggletouchpad.sh	$gitf/bin/
+cp /etc/default/grub            $gitf/etc/default/
+cp /etc/plymouth/plymouthd.conf $gitf/etc/plymouth/
+cp /etc/mkinitcpio.conf         $gitf/etc/
+cp /etc/$service/lock.service   $gitf/etc/$service/
+
+cp $lbin/togglebluetooth.sh $gitf/bin/
+cp $lbin/toggletouchpad.sh $gitf/bin/
 
 pacman -Qqe > $gitf/package_list.txt
